9891cf53d56cd4e69b8bc0f605667b05af9dd691
Saida Zghal <zghal.saida@orange.fr>
2019-08-27 23:01:02 +0200

    creation d'un 2ème questionnaire

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-var questionnaireJavascript = require("./questionnaire.json");
+// var questionnaireJavascript = require("./questionnaire.json");
+var questionnaire = selectionQuestionnaire();
 var gestionQuestion = require("./gestionQuestion");
 
 var isGameOver = false;
 
 while (!isGameOver) {
-  var question = gestionQuestion.genererQuestionAleatoire(
-    questionnaireJavascript
-  );
+  var question = gestionQuestion.genererQuestionAleatoire(questionnaire);
   gestionQuestion.afficherUneQuestion(question);
   var reponse = gestionQuestion.saisirReponse();
   var isBonneReponse = gestionQuestion.estBonneReponse(question, reponse); //renvoie un booleen
@@ -15,5 +14,14 @@ while (!isGameOver) {
   } else {
     console.log("C'est une mauvaise réponse !");
     isGameOver = true;
+  }
+}
+
+function selectionQuestionnaire() {
+  var numeroQuestionnaire = Math.floor(Math.random() * 2 + 1); // 2 pour les 2 questions et * 1 pour éviter d'avoir le 0
+  if (numeroQuestionnaire === 1) {
+    return require("./questionnaireReact.json");
+  } else {
+    return require("./questionnaireJs.json");
   }
 }
