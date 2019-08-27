234adf48c5b48ce30a7dcf94df4843d57c6abcfa
Saida Zghal <zghal.saida@orange.fr>
2019-08-27 18:51:59 +0200

    arrêt du programme à une mauvaise réponse

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 var questionnaireJavascript = require("./questionnaire.json");
 var gestionQuestion = require("./gestionQuestion");
 
-var question = gestionQuestion.genererQuestionAleatoire(
-  questionnaireJavascript
-);
+var isGameOver = false;
 
-gestionQuestion.afficherUneQuestion(question);
-
-var reponse = gestionQuestion.saisirReponse();
-
-var bool = gestionQuestion.estBonneReponse(question, reponse); //renvoie un booleen
-
-console.log(bool);
+while (!isGameOver) {
+  var question = gestionQuestion.genererQuestionAleatoire(
+    questionnaireJavascript
+  );
+  gestionQuestion.afficherUneQuestion(question);
+  var reponse = gestionQuestion.saisirReponse();
+  var isBonneReponse = gestionQuestion.estBonneReponse(question, reponse); //renvoie un booleen
+  if (isBonneReponse) {
+    console.log("C'est une bonne réponse !");
+  } else {
+    console.log("C'est une mauvaise réponse !");
+    isGameOver = true;
+  }
+}
